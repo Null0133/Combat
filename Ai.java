aaf911b2b27dee1dda3df14c01cf89b6c50e6994
Null0133 <s4251499@student.rmit.edu.au>
2026-05-30 04:29:18 +1000

    4:28
    spent most of this time browsing thu kais code trying to see how i can make the character without fucking up to much and decided to make a backup before checking if he is working on github

--- Ai.java
+++ Ai.java
@@ -1,3 +1,19 @@
 public class Ai {
     
 }
+class ai{
+    public int aiType;
+    public String name;
+    public int aiHealth;
+    public int aiDef;
+    public int aiSpeed;
+    public int aiCMV;
+
+    public ai(int aiType, String name, int aiHealth, int aiDef, int aiSpeed, int aiCMV){
+        this.name = name;
+        this.aiHealth = aiHealth;
+        this.aiDef = aiDef;
+        this.aiSpeed = aiSpeed;
+        this.aiCMV = aiCMV;
+    }
+}
--- combat.java
+++ combat.java
@@ -1,20 +1,27 @@
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
-public record diceResult(List<Integer> rolls, int sum) {}
-public class Combat{
+record diceResult(int sum, List<Integer> rolls) {}
+
+public class combat{
     public static void main(String[] args) {   //replace main with the combat start string thingy
         Cards.cards();
         System.out.println(Cards.getCardsByType(2));
     }
 
-    public static int rollDice(int num,int sides){
+    public static diceResult rollDice(int num,int sides){
+
             Random random = new Random();
             int sum = 0;
             List<Integer> Rolls = new ArrayList<>();
+
             for(int i = 0; i<num;i++){
                 int role = random.nextInt(1,sides + 1);
                 Rolls.add(role);
                 sum += role;
             }
-            return diceResult(Rolls, sum);
+
+            return new diceResult(sum, Rolls);
+    }
 }
